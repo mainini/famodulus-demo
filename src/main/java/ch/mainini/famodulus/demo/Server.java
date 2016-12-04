50a630285911245be44418784fa3727b39b948df
Pascal Mainini <pascal@mainini.ch>
2016-12-04 22:18:40 +0100

    custom handler not used anymore

--- src/main/java/ch/mainini/famodulus/demo/Server.java
+++ src/main/java/ch/mainini/famodulus/demo/Server.java
@@ -7,7 +7,9 @@ package ch.mainini.famodulus.demo;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
@@ -24,6 +26,13 @@ public class Server {
      * @todo make configurable
      */
     public static final String BASE_URI = "http://localhost:8080/";
+
+    /**
+     * Enables/disables file caching for the static handler. If disabled,
+     * changes to static content can be made at runtime which is useful for
+     * development and demonstration purposes.
+     */
+    private static final boolean ENABLE_CACHING = false;
 
     private static final Logger LOG = Logger.getLogger(Server.class.getName());
 
@@ -51,9 +60,13 @@ public class Server {
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
-        LOG.fine("Setting up handler for static content...");
+        LOG.fine("Creating server...");
         final HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI));
-        httpServer.getServerConfiguration().addHttpHandler(new CustomStaticHttpHandler(), "/");
+
+        LOG.log(Level.FINE, "Setting up handler for static content (file caching: {0})...", ENABLE_CACHING);
+        final CLStaticHttpHandler staticHandler = new CLStaticHttpHandler(Server.class.getClassLoader(), "ch/mainini/famodulus/demo/");
+        staticHandler.setFileCacheEnabled(ENABLE_CACHING);
+        httpServer.getServerConfiguration().addHttpHandler(staticHandler, "/");
 
         return httpServer;
     }
