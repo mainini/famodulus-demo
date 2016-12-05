269893dcaf6143095ac7d200dee71e98399eff98
Pascal Mainini <pascal@mainini.ch>
2016-12-05 21:13:01 +0100

    implemented calculation of single modexp, added some comfort stuff

--- src/main/resources/ch/mainini/famodulus/demo/js/util.js
+++ src/main/resources/ch/mainini/famodulus/demo/js/util.js
@@ -5,7 +5,11 @@ $('document').ready(function () {
 
     FamodulusDemo.P_3072 = '8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006119df';
 
+    FamodulusDemo.DEFAULT_RAND_LENGTH = 768;
+    FamodulusDemo.DEFAULT_SERVER = 'http://localhost:8081/api/modexp/';
+
     FamodulusDemo.randHexString = function (length) {
+        length = length === undefined ? FamodulusDemo.DEFAULT_RAND_LENGTH : length;
         var retval = '';
         while (retval.length < length) {
             var value = Math.floor(Math.random() * 256);
@@ -34,7 +38,20 @@ $('document').ready(function () {
         script.type = 'text/javascript';
         script.src = 'http://www.verificatum.com/files/vjsc-1.1.0.js';
         $("body").append(script);
+        window.BigInt.modexp = window.BigInt.modexpVerificatum;
         alert('Verificatum library injected!');  // @todo errorhandling!
+    };
+
+    FamodulusDemo.stringToList = function (val) {
+        if (val.length === 0) {
+            return [];
+        } else {
+            return val.replace(/\s+/gm, '').split(',');
+        }
+    };
+
+    FamodulusDemo.getServer = function (field) {
+        return $(field).val().length > 0 ? $(field).val() : FamodulusDemo.DEFAULT_SERVER;
     };
 
     window.FamodulusDemo = FamodulusDemo;
