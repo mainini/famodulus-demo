a48f872284b689a004d4ff14decdab73c150fd76
Pascal Mainini <pascal@mainini.ch>
2016-12-05 18:55:39 +0100

    implementation of random filling

---
+++ src/main/resources/ch/mainini/famodulus/demo/js/util.js
@@ -0,0 +1,33 @@
+$('document').ready(function () {
+    'use strict';
+
+    var FamodulusDemo = {};
+
+    FamodulusDemo.P_3072 = '8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006119df';
+
+    FamodulusDemo.randHexString = function (length) {
+        var retval = '';
+        while (retval.length < length) {
+            var value = Math.floor(Math.random() * 256);
+            retval += value < 10 ? '0' + value.toString(16) : value.toString(16);
+        }
+        return retval;
+    };
+
+    FamodulusDemo.appendTo = function (field, value) {
+        var curval = $(field).val().toString();
+        if (curval.length === 0) {
+            $(field).val(value);
+        } else if (curval.endsWith(',')) {
+            $(field).val(curval + '\n' + value);
+        } else if (curval.endsWith(',\n')) {
+            $(field).val(curval + value);
+        } else if (curval.endsWith('\n')) {
+            $(field).val(curval + ',' + value);
+        } else {
+            $(field).val(curval + ',\n' + value);
+        }
+    };
+
+    window.FamodulusDemo = FamodulusDemo;
+});
