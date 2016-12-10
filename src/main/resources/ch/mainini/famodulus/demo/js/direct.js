a7522d01d3a827c4f410cf2ecfb9deb61fe1a4d8
Pascal Mainini <pascal@mainini.ch>
2016-12-10 15:58:40 +0100

    initial implementation for multiple modexps

--- src/main/resources/ch/mainini/famodulus/demo/js/direct.js
+++ src/main/resources/ch/mainini/famodulus/demo/js/direct.js
@@ -30,7 +30,7 @@ $('document').ready(function () {
             var resultLocal = BigInt.modexp(data.modexps[0][0], data.modexps[0][1], data.modexps[0][2]);
             timeLocal = performance.now() - timeLocal;
             FD.showLocalTime(timeLocal);
-            FD.showLocalResult(resultLocal);
+            FD.showLocalResult(resultLocal, 1);
 
             // remote performance measurement
             var famodulus = new Famodulus([FD.getServer('#input-server-1')], $('#input-brief').is(':checked'));
@@ -47,12 +47,54 @@ $('document').ready(function () {
                 } else {
                     FD.showRemoteTime(timeRemote);
                 }
-                FD.showRemoteResult(result.r);
+                FD.showRemoteResult(result.r, 1);
                 FD.showDifference(timeRemote, timeLocal);
                 FD.showEqual(result.r, resultLocal);
             });
         } else {
-            alert('Not implemented yet!');
+            FD.showResults();
+
+            // local performance measurement
+            var results = [];
+            var timeLocal = performance.now();
+            for (var i = 0; i < data.modexps.length; i++) {
+                results.push(BigInt.modexp(data.modexps[i][0], data.modexps[i][1], data.modexps[i][2]));
+            }
+            timeLocal = performance.now() - timeLocal;
+
+            var resultLocal = '';
+            for (var i = 0; i < results.length - 1; i++) {
+                resultLocal += results[i] + ',\n';
+            }
+            resultLocal += results[results.length - 1];
+            FD.showLocalTime(timeLocal);
+            FD.showLocalResult(resultLocal, results.length);
+
+            // remote performance measurement
+            var famodulus = new Famodulus([FD.getServer('#input-server-1')], $('#input-brief').is(':checked'));
+            var timeRemote = performance.now();
+            famodulus.modexps(data.modexps, function (results) {
+                timeRemote = performance.now() - timeRemote;
+
+                if (timeRemote < timeLocal) {
+                    FD.showRemoteTime(timeRemote, true);
+                    FD.showLocalTime(timeLocal, false);
+                } else if (timeRemote > timeLocal) {
+                    FD.showRemoteTime(timeRemote, false);
+                    FD.showLocalTime(timeLocal, true);
+                } else {
+                    FD.showRemoteTime(timeRemote);
+                }
+
+                var resultRemote = '';
+                for (var i = 0; i < results.length - 1; i++) {
+                    resultRemote += results[i].r + ',\n';
+                }
+                resultRemote += results[results.length - 1].r;
+                FD.showRemoteResult(resultRemote, results.length);
+                FD.showDifference(timeRemote, timeLocal);
+                FD.showEqual(resultRemote, resultLocal);
+            });
         }
     });
 
@@ -96,7 +138,7 @@ $('document').ready(function () {
     reset();
     $('#input-server-1').attr('placeholder', FD.DEFAULT_SERVER);
 
-    if(typeof(verificatum) !== 'undefined') {
+    if (typeof (verificatum) !== 'undefined') {
         $('#btn-verificatum').prop('disabled', true);
     } else {
         $('#btn-verificatum').prop('disabled', false);
